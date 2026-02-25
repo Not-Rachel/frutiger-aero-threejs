61562f43c6c45fc591d1db0b238c9d04c216ac3a
Rachel Brinkman <98926561+Not-Rachel@users.noreply.github.com>
2026-02-24 20:22:07 -0800

    YUKA pathfinding

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,11 @@ function App() {
 
   return (
     <div className="flex items-center justify-center h-full w-full">
+      <div className="flex flex-col gap-4 absolute p-6 z-99 text-white">
+        <button className="aero">Projects</button>
+        <button className="aero">Resume</button>
+        <button className="aero">About</button>
+      </div>
       <Background></Background>
       {/* <div className="border-2 border-white w-1/3 h-1/2 flex justify-center items-center m-4 p-4 flex-col"></div> */}
     </div>
--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,4 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "@react-three/fiber";
+
 import hdr from "../assets/puresky.hdr?url";
 import {
   Environment,
@@ -10,68 +12,129 @@ import {
 } from "@react-three/drei";
 import fishModel from "../assets/scene.gltf?url";
 import puterModel from "../assets/retroComputer.gltf?url";
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import * as YUKA from "yuka";
 import { useEffect, useMemo, useRef } from "react";
 import { SkeletonUtils } from "three/examples/jsm/Addons.js";
+import { Water, type WaterOptions } from "three/examples/jsm/Addons.js";
+import {
+  DepthOfField,
+  EffectComposer,
+  Noise,
+  HueSaturation,
+} from "@react-three/postprocessing";
+
 function randInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 function FishModel(props) {
   const gltf = useGLTF(fishModel);
   const cloned = useMemo(() => SkeletonUtils.clone(gltf.scene), [gltf.scene]);
+  // cloned.matrixAutoUpdate = false;
 
   const { actions } = useAnimations(gltf.animations, cloned);
-  const fishRef = useRef(null);
-  const timeRef = useRef(0);
-  const directionRef = useRef(1);
-  const speed = 0.05; // Adjust for larger/smaller circle
-  const borderRadius = 10.0;
+  const fishRef = useRef<THREE.Group>(null);
+  const vehicleRef = useRef<YUKA.Vehicle>(null!);
+  const previousTimeRef = useRef<number>(0);
+  const speedRef = useRef(props.speed); // Adjust for larger/smaller circle
+  const borderRadius = 20.0;
+  const targetBehaviorRef = useRef<YUKA.ArriveBehavior>(null!);
+  const wanderBehaviorRef = useRef<YUKA.WanderBehavior>(null!);
 
   useEffect(() => {
+    if (fishRef.current) {
+      //Connect mesh to moving obj
+      const vehicle = new YUKA.Vehicle();
+      vehicle.setRenderComponent(fishRef.current);
+      vehicle.position.set(
+        props.position[0],
+        props.position[1],
+        props.position[2],
+      );
+      vehicle.maxSpeed = speedRef.current;
+      vehicle.maxForce = 8;
+      vehicle.mass = 2;
+
+      // vehicle.position.copy({
+      //   x: props.position[0],
+      //   y: props.position[1],
+      //   z: props.position[2],
+      // });
+
+      //Behavior
+      const wander = new YUKA.WanderBehavior();
+      wander.radius = 5;
+      wander.distance = 10;
+      wander.jitter = 4;
+      const target = new YUKA.ArriveBehavior(new YUKA.Vector3(0, 0, 0), 2, 2);
+      target.active = false;
+
+      // wander.active = false;
+      vehicle.steering.add(wander);
+      vehicle.steering.add(target);
+
+      targetBehaviorRef.current = target;
+      wanderBehaviorRef.current = wander;
+      // entityManager.add(vehicle);
+
+      vehicleRef.current = vehicle;
+    }
+
     const action = actions["Swim Animation"];
     if (action) {
-      action.timeScale = 0.2;
+      action.timeScale = speedRef.current / 7.0;
       action.play();
     }
-  }, [actions]);
+  }, []);
 
-  useFrame(() => {
-    if (fishRef && fishRef.current) {
-      const posX = fishRef.current.position.x;
-      const posZ = fishRef.current.position.z;
+  useFrame(({ clock }) => {
+    if (fishRef.current && vehicleRef.current) {
+      const currentTime = clock.getElapsedTime();
+      const delta = currentTime - previousTimeRef.current;
+      previousTimeRef.current = currentTime;
 
-      const radius = Math.sqrt(posX * posX + posZ * posZ);
+      const { x, z } = fishRef.current.position;
+      const distance = Math.sqrt(x * x + z * z);
 
-      //teleport back
-      if (radius - borderRadius > 1) {
-        fishRef.current.position.x = 0;
-        fishRef.current.position.z = 0;
-        return;
-      }
-      // Fish should try to rotate away from border
-      const rotationMult = Math.abs(2 / (borderRadius - radius)); // Start rotating more as the fish gets to the edge
-      timeRef.current += 0.01;
-
-      // Sometimes change direction
-      if (Math.abs(rotationMult) < 0.5 && Math.random() < 0.01) {
-        directionRef.current = -directionRef.current;
+      if (distance > borderRadius) {
+        wanderBehaviorRef.current!.active = false;
+        targetBehaviorRef.current!.active = true;
+      } else {
+        wanderBehaviorRef.current!.active = true;
+        targetBehaviorRef.current!.active = false;
       }
 
-      // Rotate fish to face direction of movement
-      fishRef.current.rotation.y =
-        (fishRef.current.rotation.y +
-          0.03 * directionRef.current * rotationMult) %
-        (Math.PI * 2);
+      vehicleRef.current.update(delta);
 
-      const movex = Math.sin(fishRef.current.rotation.y) * speed;
-      const movey = Math.cos(fishRef.current.rotation.y) * speed;
+      // Manual sync if needed
+      fishRef.current.position.copy(vehicleRef.current.position);
+      fishRef.current.quaternion.copy(vehicleRef.current.rotation);
 
-      fishRef.current.position.x += movex;
-      fishRef.current.position.z += movey;
-      fishRef.current.position.y += 0.002 * directionRef.current;
+      // const verticalOffset = Math.sin(clock.getElapsedTime() * 0.5) * 0.5;
+      // fishRef.current.position.y = verticalOffset;
+
+      // // Fish should try to rotate away from border
+      // const rotationMult = Math.abs(2 / (borderRadius - radius)); // Start rotating more as the fish gets to the edge
+
+      // // Sometimes change direction
+      // if (Math.abs(rotationMult) < 0.5 && Math.random() < 0.01) {
+      //   directionRef.current = -directionRef.current;
+      // }
+
+      // // Rotate fish to face direction of movement
+      // fishRef.current.rotation.y =
+      //   (fishRef.current.rotation.y +
+      //     0.03 * directionRef.current * rotationMult) %
+      //   (Math.PI * 2);
+
+      // const movex = Math.sin(fishRef.current.rotation.y) * speed;
+      // const movey = Math.cos(fishRef.current.rotation.y) * speed;
+
+      // fishRef.current.position.x += movex;
+      // fishRef.current.position.z += movey;
+      // fishRef.current.position.y += 0.002 * directionRef.current;
     }
   });
 
@@ -82,52 +145,95 @@ function PuterModel(props) {
   const gltf = useGLTF(puterModel);
   const puterRef = useRef(null);
   if (puterRef && puterRef.current) {
-    console.log("Puter pos", puterRef.current.position, puterRef.current.scale);
+    // console.log("Puter pos", puterRef.current.position, puterRef.current.scale);
   }
 
-  return <primitive ref={puterRef} object={gltf.scene} {...props} />;
+  return (
+    <primitive
+      ref={puterRef}
+      object={gltf.scene}
+      {...props}
+      onClick={(e: Event) => {
+        e.stopPropagation();
+        console.log("Clicked computer");
+      }}
+    />
+  );
 }
 
+function BubbleMesh(props) {
+  const bubble = useRef(null!);
+
+  useFrame(({ clock }) => {
+    if (bubble.current) {
+      bubble.current.position.y += 0.001;
+    }
+    // console.log("Frame");
+  });
+
+  return (
+    <Float floatIntensity={1} speed={0.5}>
+      <mesh ref={bubble} scale={1} position={[0, 0, 0]}>
+        <sphereGeometry args={[1, 64, 64]} />
+        <MeshDistortMaterial
+          distort={0.25}
+          transmission={1.0}
+          thickness={-0.5}
+          roughness={0}
+          iridescence={1}
+          iridescenceIOR={1}
+          iridescenceThicknessRange={[0, 1200]}
+          clearcoat={1}
+          clearcoatRoughness={0}
+          envMapIntensity={1.5}
+        />
+      </mesh>
+    </Float>
+  );
+}
 function Bubble() {
   // const gltf = useLoader(GLTFLoader, gltf_model);
 
   return (
     <Canvas camera={{ fov: 65 }} className="border-2 border-white">
-      {/* <primitive object={gltf.scene} /> */}
-      {/* <FishModel scale={7} position={[1, 1, 0]} /> */}
-      {/* <FishModel scale={7} position={[0, 0, 0]} /> */}
-      {Array.from({ length: 8 }).map((_, i) => (
+      {Array.from({ length: 20 }).map((_, i) => (
         <FishModel
           key={i}
           scale={7}
-          position={[randInt(-5, 5), randInt(-1, 1), randInt(-5, 5)]}
+          // position={[randInt(-5, 5), randInt(-1, 1), randInt(-5, 5)]}
+          speed={randInt(1, 5)}
+          position={[0, 0, 0]}
         />
       ))}
       <PuterModel
         scale={4}
-        position={[-3, 0, -5]}
-        rotation={[0, -0.25 * Math.PI, Math.PI / 12]}
+        position={[5, 0, -7]}
+        rotation={[0, -Math.PI / 1.3, Math.PI / 12]}
       />
+      <BubbleMesh />
+      <mesh scale={1} position={[0, 0, 0]}>
+        <planeGeometry args={[64, 64]} rotateX={-Math.PI / 2} />
+        <MeshDistortMaterial
+          distort={0.0}
+          transmission={1.0}
+          thickness={-0.5}
+          roughness={0}
+          iridescence={1}
+          iridescenceIOR={1}
+          iridescenceThicknessRange={[0, 1200]}
+          clearcoat={1}
+          clearcoatRoughness={0}
+          envMapIntensity={1.5}
+        />
+      </mesh>
 
-      <Float floatIntensity={1} speed={0.5}>
-        <mesh scale={1}>
-          <sphereGeometry args={[1, 64, 64]} />
-          <MeshDistortMaterial
-            distort={0.25}
-            transmission={1.0}
-            thickness={-0.5}
-            roughness={0}
-            iridescence={1}
-            iridescenceIOR={1}
-            iridescenceThicknessRange={[0, 1200]}
-            clearcoat={1}
-            clearcoatRoughness={0}
-            envMapIntensity={1.5}
-          />
-        </mesh>
-      </Float>
-
-      <OrbitControls enableZoom={true} />
+      <OrbitControls
+        enableZoom={true}
+        // minAzimuthAngle={-Math.PI / 4}
+        // maxAzimuthAngle={Math.PI / 4}
+        // minPolarAngle={Math.PI / 6}
+        // maxPolarAngle={Math.PI - Math.PI / 6}
+      />
       <Environment
         files={hdr}
         backgroundBlurriness={0.03}
@@ -135,6 +241,17 @@ function Bubble() {
         far={100}
         background={true}
       />
+      <EffectComposer>
+        <DepthOfField
+          // focusDistance={2}
+          worldFocusDistance={10}
+          focalLength={20}
+          bokehScale={3}
+          // height={1000}
+        />
+        <HueSaturation staturation={1} />
+        <Noise opacity={0.02} />
+      </EffectComposer>
     </Canvas>
   );
 }
