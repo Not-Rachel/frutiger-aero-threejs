4a013ab63145a0c4d79a1ee7534e1844ee76f0ee
Rachel Brinkman <98926561+Not-Rachel@users.noreply.github.com>
2026-03-12 12:03:48 -0700

    added scavenger dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/threejs": {
-        target: "http://localhost:5175/threejs/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/threejs/, ""),
-        followRedirects: true,
-      },
-      "/scavenger": {
-        target: "http://localhost:5174/scavenger",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/scavenger/, ""),
-        followRedirects: true,
-      },
-    },
-  },
 });
