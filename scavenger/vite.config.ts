3bb271688ddbff63988c2a24f357a3c8f968241d
Rachel Brinkman <98926561+Not-Rachel@users.noreply.github.com>
2026-03-13 14:11:03 -0700

    Fixed building

--- scavenger/vite.config.ts
+++ scavenger/vite.config.ts
@@ -6,4 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/scavenger/",
+  build: {
+    outDir: "../dist/scavenger",
+  },
 });
